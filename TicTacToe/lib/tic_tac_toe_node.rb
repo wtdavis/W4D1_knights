3aa9799076126714dd9d15077ede6fcc2aa30b5a
wtdavis <wtdavis@wesleyan.edu>
2023-01-30 17:17:55 -0500

    done for now- instantiate children

--- TicTacToe/lib/tic_tac_toe_node.rb
+++ TicTacToe/lib/tic_tac_toe_node.rb
@@ -2,7 +2,13 @@ require_relative 'tic_tac_toe'
 
 class TicTacToeNode
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
+
   end
+
+  
 
   def losing_node?(evaluator)
   end
@@ -13,5 +19,26 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    childrens = []
+    board.each_with_index do |row, rowidx|
+      row.each_with_index do |col, colidx|
+        if col == nil
+
+          childrens << TicTacToeNode(board.dup, <switched_mover_mark>, prev_move_pos = nil)
+        tempnode.board([rowidx,colidx]) = <next_mover_mark>
+        # change <next_mover_mark> to another
+      end
+    end
   end
+
+  def switch_symbol
+    if next_mover_mark == :x
+      next_mover_mark = :o
+    else
+      next_mover_mark = :x
+    end
+  end
+
+
+
 end
